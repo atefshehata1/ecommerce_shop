776fa168c67930d2191c90e1e5cd1b314a319a38
atefshehata1 <atefshehata52@gmail.com>
2025-09-19 18:03:30 +0300

    fix: Correct token session name for production environment

--- src/getUserToken.ts
+++ src/getUserToken.ts
@@ -5,7 +5,7 @@ import { cookies } from 'next/headers'
 
 export default async function getUserToken() {
   try {
-    const TokenSession = (process.env.NODE_ENV === "production" ?  '_Secure-next-auth.session-token' : "next-auth.session-token" )
+    const TokenSession = (process.env.NODE_ENV === "production" ?  '__Secure-next-auth.session-token' : "next-auth.session-token" )
     const cookiesData = await cookies()
     const encryptToken = cookiesData.get(TokenSession)?.value
     if (!encryptToken) {
